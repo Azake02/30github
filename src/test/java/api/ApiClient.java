97f828a2c4b1d66f73bac4e14217b07ff3dde615
Azat <azat.azamatov.02@bk.ru>
2025-02-08 21:35:45 +0500

    added ApiClient

---
+++ src/test/java/api/ApiClient.java
@@ -0,0 +1,23 @@
+package api;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiClient {
+    public static Response get(String endpoint){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(endpoint);
+    }
+
+    public static Response post(String endpoint, Object body){
+        return given()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when().
+                post(endpoint);
+    }
+}
--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import api.ApiClient;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -8,9 +9,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class ApiTest extends BaseTest{
     @Test
     void testId(){
-        given()
-                .when()
-                .get("posts/1")
+        ApiClient.get("posts/1")
                 .then()
                 .statusCode(200)
                 .body("id", equalTo(1));
