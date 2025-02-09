588a0f2d5b05fac4f2ab3d67f703e4595ebc7469
Azat <azat.azamatov.02@bk.ru>
2025-02-09 21:20:05 +0500

    made Comment test parametrized

--- src/test/java/tests/CommentTests.java
+++ src/test/java/tests/CommentTests.java
@@ -2,18 +2,20 @@ package tests;
 
 import api.ApiClient;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class CommentTests extends BaseTest{
     private static final String COMMENTS_ENDPOINT = "/comments";
 
-    @Test
-    void getCommentById(){
-        Response response = ApiClient.get(COMMENTS_ENDPOINT + "/1");
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void getCommentById(int id){
+        Response response = ApiClient.get(COMMENTS_ENDPOINT + "/" + id);
         response.then()
                 .statusCode(200)
-                .body("id", equalTo(1));
+                .body("id", equalTo(id));
     }
 }
