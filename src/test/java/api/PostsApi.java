75eebad8b02bc64f976a2709a0516fb2687ac8d3
Azat <azat.azamatov.02@bk.ru>
2025-02-11 21:35:42 +0500

    added dto and test for posts

--- src/test/java/api/PostsApi.java
+++ src/test/java/api/PostsApi.java
@@ -1,5 +1,35 @@
 package api;
 
-public class PostsApi {
+import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.given;
+
+import dto.PostDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class PostsApi {
+    private static final String POSTS_ENDPOINT = "/posts";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static Response createPost(PostDto post) {
+        String requestBody;
+        try {
+            requestBody = objectMapper.writeValueAsString(post);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize PostDto", e);
+        }
+
+        return given()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .when()
+                .post(POSTS_ENDPOINT);
+    }
+
+    public static Response deletePost(int postId) {
+        return given()
+                .when()
+                .delete(POSTS_ENDPOINT + "/" + postId);
+    }
 }
