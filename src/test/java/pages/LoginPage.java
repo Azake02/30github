0d520a8dfb6f2d9832ccf00869a447dd61761fca
Azat <azat.azamatov.02@bk.ru>
2025-02-17 22:10:08 +0500

    added config for BaseURL

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,11 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LoginPage {
-    private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
     protected WebDriver driver;
 
     @FindBy(xpath = "//input[@id='idToken1']")
@@ -20,18 +17,11 @@ public class LoginPage {
     @FindBy(xpath = "//input[@id='idToken2']")
     private WebElement loginBtn;
 
-
-    @FindBy(xpath = "(//button[@type='button'])[1]")
-    private WebElement dashboard;
-
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
-    public void clickOnLogInBtn(){
-        dashboard.click();
-    }
     public void login(String username, String password) {
         usernameField.click();
         usernameField.sendKeys(username);
--- src/test/java/ui/tests/LoginTest.java
+++ src/test/java/ui/tests/LoginTest.java
@@ -9,23 +9,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
+import utils.Config;
 
 public class LoginTest {
     private WebDriver driver;
     private LoginPage loginPage;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws InterruptedException {
         driver = new ChromeDriver();
-        driver.get("https://saucelabs.com/");
+        driver.get(Config.BASE_URL);
+        driver.wait(5000);
         loginPage = new LoginPage(driver);
     }
 
 
     @Test
     void testLogin() {
-        loginPage.clickOnLogInBtn();
-        loginPage.login("user", "azat@gmail.com");
+        loginPage.login("Azat", "Amina21_");
     }
 
     @AfterEach
